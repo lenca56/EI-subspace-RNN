75e0fda9489388ea9cf7182fc3fe379d1f23b9f1
Lenca Cuturela <cuturela@princeton.edu>
2025-01-23 17:25:07 -0500

    fixed error for cluster fitting bcs of shape of J

--- fit_cluster_RNN_fixed_N.py
+++ fit_cluster_RNN_fixed_N.py
@@ -10,9 +10,9 @@ import matplotlib.pyplot as plt
 import sys
 import os
 
-df = pd.DataFrame(columns=['K','simulation']) # in total z=0,239 
+df = pd.DataFrame(columns=['K','simulation']) # in total z=0,209 
 z = 0 
-for K in [1,2,3,4,5,10,25,40]:
+for K in [2,3,4,5,10,25,40]:
     for simulation in range(30):
         df.loc[z, 'K'] = K
         df.loc[z, 'simulation'] = simulation
@@ -48,8 +48,8 @@ J_possibilities.append(J)
 J = np.random.normal(0, 1/np.sqrt(N), (N,N))
 # J, _ = np.linalg.qr(J)  # QR decomposition, Q is the orthogonal matrix
 if K > 1:
-    J = J[:K-1,:]
-    J[-1,:] = 1/np.sqrt(N)
+    J = J[:K,:]
+    J[K-1,:] = 1/np.sqrt(N)
 else:
     J = J[0,:].reshape((1,N))
     J[0,:] = 1/np.sqrt(N)
@@ -61,6 +61,7 @@ J = np.random.uniform(0, 1, (N,N))
 J = J[:K,:] / 1/np.sqrt(N)
 J_possibilities.append(J)
 
+# generate dynamics
 eigenvalues = generate_eigenvalues(K)
 trueA = generate_dynamics_A(eigenvalues) 
 
@@ -69,7 +70,7 @@ for i in range(len(J_possibilities)):
     J_pinv = np.linalg.pinv(J) # pseudo-inverse (J * J_inv = identity, but J_inv * J is not)
     RNN = EI_subspace_RNN.EI_subspace_RNN(N_e, N_i, sparsity, J, seed=1)
     zeta_alpha_beta_gamma_list = [(10**i,1,1,0) for i in list(np.arange(-1,0.5,0.5))]
-    initW0, initW, loss_W, w_all = RNN.generate_or_initialize_weights_from_dynamics_LDS(A_target=trueA, R=0.85, zeta_alpha_beta_gamma_list = zeta_alpha_beta_gamma_list)
+    initW0, initW, loss_W, w_all = RNN.generate_or_initialize_weights_from_dynamics_LDS(A_target=trueA, R=0.85)#, zeta_alpha_beta_gamma_list = zeta_alpha_beta_gamma_list)
     init_w = RNN.get_nonzero_weight_vector(initW)
     initA = build_dynamics_matrix_A(initW, J)
 
@@ -80,11 +81,3 @@ for i in range(len(J_possibilities)):
 
     np.savez(f'models/N={N}_K={K}_parameters_simulation_{simulation}_J_possibility_{i}', ecll=ecll, ll=ll, initW0=initW0, initW =initW, loss_W = loss_W, w_all = w_all, lossW=lossW, w=w, b=b, s=s, mu0=mu0, Q0=Q0, C_=C_, d=d, R=R, J=J, true_x=true_x, true_y=true_y, trueA=trueA, true_b=true_b, true_s=true_s, true_mu0=true_mu0, true_Q0=true_Q0, true_C_=true_C_, true_d=true_d, true_R=true_R)
     
-
-# if simulation == 0: # initialize parameters from true ones
-#     max_iter = 20
-#     lossW, w, b, s, mu0, Q0, C_, d, R = RNN.fit_EM(true_y, init_w, true_b, true_s, true_mu0, true_Q0, true_C_, true_d, true_R, alpha=10, beta=10, max_iter=max_iter)
-# else:
-#     max_iter = 200
-#     init_b, init_s, init_mu0, init_Q0, init_C_, init_d, init_R = RNN.generate_parameters(D, K)
-#     lossW, w, b, s, mu0, Q0, C_, d, R = RNN.fit_EM(true_y, init_w, init_b, init_s, init_mu0, init_Q0, init_C_, init_d, init_R, alpha=10, beta=10, max_iter=max_iter)
